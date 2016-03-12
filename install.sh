239cec2c03bbe5df4b00f019424525842cdd7f25
catchwhale <mrjoe8888@gmail.com>
2016-03-12 19:43:04 +0800

    updating install sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-
-echo ' $(logname) ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+name=`logname`
+echo "$name ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 cp .0 /usr/bin/0
 chmod +x /usr/bin/0
 
